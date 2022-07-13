f4c565d24f6c83eefbb7e51c58996561f95c5951
J Semidey <mtsemidey@gmail.com>
2022-07-13 02:11:28 -0400

    set up skeleton of user model, with bcrypt and virtual for friendCount

---
+++ server/models/User.js
@@ -0,0 +1,73 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// define User schema
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // regex to match standard email addresses
+      match: [/.+@.+\..+/, 'Must match an email address!']
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5
+    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ],
+    ratedMovies: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Movie'
+      }
+    ],
+    suggestedMovies: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Movie'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
+
+// use pre-save middleware to create password with bcrypt
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare incoming password with hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+// virtual for total number of friends
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+// export
+module.exports = User;
