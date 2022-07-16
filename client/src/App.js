4e22ef3f288824a26ae581ae4523661023d6a589
J Semidey <mtsemidey@gmail.com>
2022-07-16 12:02:03 -0400

    updated react app to handle apolloprovider

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+// base css script
 import './index.css';
+
+// create connection to backend server
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql'
+});
+
+// instantiate apollo client instance and create connection to the API endpoint
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client} >
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    </ApolloProvider>
   );
 }
 
