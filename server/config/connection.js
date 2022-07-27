92bb5fd0f16ee23bb7bf631b99d16afd75681b14
J Semidey <mtsemidey@gmail.com>
2022-07-27 01:19:03 -0400

    deploying to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/popcornpals', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/popcornpalsDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
